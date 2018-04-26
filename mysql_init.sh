a416821411edcec60fdeeed52edaf70ab1b18c3c
david <david.schick@online.de>
2018-04-26 20:15:38 +0200

    changed constant values into variables

--- mysql_init.sh
+++ mysql_init.sh
@@ -2,7 +2,8 @@
 
 containerName=todomysql
 dbname=tododb
-
+rootpw=root
+port=3306
 
 query="create user 'vertsys'@'%' identified with sha256_password by 'vertsys'; grant all privileges on tododb.* to 'vertsys'@'%'; exit;";
 
@@ -15,19 +16,19 @@ then
 fi
 if [ "$(docker container ls | grep $containerName -c )" = 0 ]
 then
-	container="$(docker run -d --name $containerName -e MYSQL_ROOT_PASSWORD='root' -e MYSQL_DATABASE=$dbname  -p 3306:3306 mysql --default-authentication-plugin=sha256_password )";
+	container="$(docker run -d --name $containerName -e MYSQL_ROOT_PASSWORD='$rootpw' -e MYSQL_DATABASE='$dbname'  -p $port:3306 mysql --default-authentication-plugin=sha256_password )";
 else
-	docker start todomysql;
+	docker start $containerName;
 fi
 
 
 
-while ! (mysqladmin ping -h 127.0.0.1 -P 3306 --protocol TCP --password=root > /dev/null 2> /dev/null) do
+while ! (mysqladmin ping -h 127.0.0.1 -P $port --protocol TCP --password=$rootpw > /dev/null 2> /dev/null) do
 	echo "Waiting for database to start...";
 	sleep 2;
 done
 
 
-docker exec todomysql bash -c 'mysql || echo $query | mysql --password=root';
+docker exec todomysql bash -c 'mysql || echo $query | mysql --password=$rootpw';
 
 
