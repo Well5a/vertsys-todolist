c418c66885a2ee22a560448bfedc33b5ade73c36
Well5a <marcelfrederik@aol.de>
2018-04-30 11:49:47 +0200

    added security login

--- mysql_init.sh
+++ mysql_init.sh
@@ -14,16 +14,14 @@ if [ "$(docker images | grep -c 'mysql')" = 0 ]
 then
 	docker pull mysql;
 fi
-if [ "$(docker container ls | grep $containerName -c )" = 0 ]
+if [ "$(docker container ls -a | grep $containerName -c )" = 0 ]
 then
-	container="$(docker run -d --name $containerName -e MYSQL_ROOT_PASSWORD=$rootpw -e MYSQL_DATABASE=$dbname  -p $port:3306 mysql --default-authentication-plugin=sha256_password )";
-else
-	docker start $containerName;
+	container="$(docker run -d --name $containerName -e MYSQL_ROOT_PASSWORD=$rootpw -e MYSQL_DATABASE=$dbname  -p $port:3306 mysql  )";
 fi
 
+docker start $containerName;
 
-
-while ! (mysqladmin ping -h 127.0.0.1 -P $port --protocol TCP --password=$rootpw > /dev/null 2> /dev/null) do
+while ! (mysqladmin ping -u root -h 127.0.0.1 -P $port --protocol=TCP --password=$rootpw ) do
 	echo "Waiting for database to start...";
 	sleep 2;
 done
--- src/main/java/de/hsesslingen/ToDoApplication.java
+++ src/main/java/de/hsesslingen/ToDoApplication.java
@@ -1,10 +1,12 @@
 package de.hsesslingen;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @ComponentScan("de.hsesslingen")
 public class ToDoApplication {
 
